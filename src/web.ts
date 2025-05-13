5c0342b2c15bb125f827b01792162f13e9a49373
kenzykozyma <kojima.kenji@amsinc.co.jp>
2025-05-13 14:33:37 +0900

    Refactor FaceidWeb.authenticate method to throw an error for unimplemented functionality

--- src/web.ts
+++ src/web.ts
@@ -3,8 +3,7 @@ import { WebPlugin } from '@capacitor/core';
 import type { FaceidPlugin } from './definitions';
 
 export class FaceidWeb extends WebPlugin implements FaceidPlugin {
-  async authenticate(options: { value: string }): Promise<{ value: string }> {
-    console.log('ECHO', options);
-    return options;
+  async authenticate(_options: { value: string }): Promise<{ value: string }> {
+    throw new Error('Method not implemented.');
   }
 }
